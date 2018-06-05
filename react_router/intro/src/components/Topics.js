6af32418833548f07209668b76c4c9a870772b0a
Bhavik Patel <Bhavikpatel576@gmail.com>
2018-06-05 11:54:01 -0500

    function version of react router

--- react_router/intro/src/components/Topics.js
+++ react_router/intro/src/components/Topics.js
@@ -1,6 +1,40 @@
 import React from 'react'
+import {
+  Route,
+  Link,
+} from 'react-router-dom'
 
+function Topic({match}) {
+  return (
+    <div>
+      <ul>
+        <li>{match.params.topicID}</li>
+      </ul>
+    </div>
+  )
+}
 
-export default function Topics () {
-  return <div>Topics</div>
+export default function Topics ({match}) {
+  return(
+    <div>
+      <h2>Topics</h2>
+        <ul>
+          <li>
+            <Link to= {`${match.url}/rendering`}>Rendering with React </Link>
+          </li>
+          <li>
+            <Link to= {`${match.url}/components`}> Componenents </Link>
+          </li>
+          <li>
+            <Link to= {`${match.url}/props-v-state`}>Props vs State </Link>
+          </li>
+        </ul>
+
+        <hr />
+        <Route path={`${match.path}/:topicID`} component={Topic} />
+        <Route exact path={match.path} render={() => {
+          return <h3>Please select a topic</h3>
+        }} />
+    </div>
+  ) 
 }
